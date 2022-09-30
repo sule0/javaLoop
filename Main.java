9410a9c7fac9d347c6838d20f39a2f6eff5f1d1e
sule0 <99013587+sule0@users.noreply.github.com>
2022-09-30 12:22:54 +0300

    loopdemo
    for loop and while loop

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// for
+        for (int i=1;i<10;i+=2) {
+        	System.out.println(i);
+        	
+        }
+        System.out.println("The For Loop is over ");
+        
+        
+        //While
+        int a=3;
+        while ( a<10) {
+        	System.out.println(a);
+        	a+=1;
+        	
+        }
+        System.out.println("The While Loop is over ");
+        
+   }
+
+}
